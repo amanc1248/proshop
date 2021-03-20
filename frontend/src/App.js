c0dea4fd2b68eec07db2710433e140b6e75329e2
amanc1248 <amanc1248@gmail.com>
2021-03-20 19:29:38 +0545

    Add: Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <>
-      <h1>welcome to prohsop</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <h1>welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 };
