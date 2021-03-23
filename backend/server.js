5c71955e05c17440874dd903c7b5a16360cd4bc7
amanc1248 <amanc1248@gmail.com>
2021-03-23 08:36:34 +0545

    ADD: redux actions and reducers

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 import express from "express";
 import colors from "colors";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleWare/middleWare.js";
 dotenv.config();
 connectDB();
 const app = express();
+app.use((req, res, next) => {
+  console.log("hello");
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(
   PORT,
   console.log(
