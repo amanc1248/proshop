bd89b32ca9e2c34e2df3249cc2dda8ee280c8710
amanc1248 <amanc1248@gmail.com>
2021-03-23 19:50:06 +0545

    ADD: Product Controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,12 @@
 import express from "express";
 const router = express.Router();
-import Product from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
 
-//@desc fetch all products
-//@route GET /api/products
-//@access PUBLIC
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
-//@desc fetch single products
-//@route GET /api/products/:id
-//@access PUBLIC
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
+
 export default router;
