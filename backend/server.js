d0f592f2e2db472405b0b8a84754ad27653d1677
amanc1248 <amanc1248@gmail.com>
2021-03-24 11:15:36 +0545

    ADD: Authentication for the user

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleWare/middleWare.js";
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json());
 app.use((req, res, next) => {
   console.log("hello");
   next();
@@ -16,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
