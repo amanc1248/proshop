2f485d9f40c80bb54df2e153ee33220492dfa19f
amanc1248 <amanc1248@gmail.com>
2021-03-25 20:08:46 +0545

    ADD: Login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 const App = () => {
   return (
     <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/login" component={LoginScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>{" "}
+          <Route path="/" component={HomeScreen} exact></Route>
         </Container>
       </main>
       <Footer></Footer>
