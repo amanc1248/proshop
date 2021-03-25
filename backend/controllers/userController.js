fe7d7f2f052353cef095e9794c9124d879a8c00e
amanc1248 <amanc1248@gmail.com>
2021-03-25 16:30:47 +0545

    ADD: Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,4 +21,52 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+//@desc get user profile
+//@route GET api/users/profile
+//@access PRIVATE
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+//@desc register a new user
+//@route POST /api/users
+//@access PUBLIC
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (useExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
