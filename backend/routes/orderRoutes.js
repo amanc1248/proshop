c0c38c407fe4d333c7e3fc7154a1aec4a54bcae6
amanc1248 <amanc1248@gmail.com>
2021-03-29 11:04:15 +0545

    ADD: GET Order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import { protect } from "../middleWare/authMiddleWare.js";
 const router = express.Router();
 
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 export default router;
