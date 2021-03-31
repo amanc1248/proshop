2f290f9d7343fd81a3add29e1d8d69062a3bd594
amanc1248 <amanc1248@gmail.com>
2021-03-31 11:18:34 +0545

    ADD: Backend of create and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,13 +5,16 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { isAdmin, protect } from "../middleWare/authMiddleWare.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, isAdmin, deleteProduct);
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
