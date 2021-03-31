6b2f7fede62751c4afdc2b60b2ea2d06fab49110
amanc1248 <amanc1248@gmail.com>
2021-03-31 08:43:54 +0545

    ADD: ProductListScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceorderScreen from "./screens/PlaceorderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 const App = () => {
   return (
     <Router>
@@ -30,6 +31,10 @@ const App = () => {
           <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/cart/:id?" component={CartScreen}></Route>{" "}
           <Route path="/admin/userlist" component={UserListScreen}></Route>{" "}
+          <Route
+            path="/admin/productlist"
+            component={ProductListScreen}
+          ></Route>{" "}
           <Route path="/admin/user/:id/edit" component={UserEditScreen}></Route>{" "}
           <Route path="/" component={HomeScreen} exact></Route>
         </Container>
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,10 +7,11 @@ import {
   PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants.js";
 import axios from "axios";
+
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("api/products");
+    const { data } = await axios.get("/api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
@@ -22,6 +23,7 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
 export const listProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
